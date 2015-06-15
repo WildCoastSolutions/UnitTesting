25ac784894b619493185d566dc33ae5f6277a5c5
wc-dha <david.hamilton@wildcoast.io>
2015-06-16 02:07:55 +1200

    Added source code and build files for unit testing library.

---
+++ Test/UnitTesting.Test.cpp
@@ -0,0 +1,57 @@
+
+
+#include "UnitTesting.h"
+
+
+using namespace Wild::UnitTesting;
+
+
+void ThrowsInvalidArgument()
+{
+	throw std::invalid_argument("foo");
+}
+
+void ThrowsOutOfRange()
+{
+	throw std::out_of_range("foo");
+}
+
+int main(int argc, char* argv[])
+{
+	AssertEquals(13, 13);
+	AssertEquals(true, true);
+	AssertEquals(false, false);
+	std::string s = "foo";
+	AssertEquals("foo", s);
+
+	Test test;
+	test.Assert(true, "filename", 1, "passed");
+	AssertEquals(1, test.passed);
+	AssertEquals(0, test.failed);
+
+	std::stringstream output;
+	std::streambuf* original = std::cout.rdbuf(output.rdbuf());
+
+	test.Assert(false, "filename", 2, "false == false");
+	std::string text = output.str();
+	output.str("");
+
+	Fail("test"); std::string failLine = std::string(__FILE__) + "(" + std::to_string(__LINE__) + ")";
+	std::string text2 = output.str();
+	
+	std::cout.rdbuf(original);
+
+	AssertEquals(text, "filename(2): test failed, tried \"false == false\"\n");
+	AssertEquals(text2, failLine + ": test failed, tried \"test\"\n");
+
+	AssertEquals(1, test.passed);
+	AssertEquals(1, test.failed);
+
+	AssertThrows(ThrowsInvalidArgument(), std::invalid_argument);
+	AssertThrows(ThrowsInvalidArgument(), std::logic_error);		// Parent classes are also accepted
+	AssertThrows(ThrowsInvalidArgument(), std::exception);			// Parent classes are also accepted
+	AssertThrows(ThrowsOutOfRange(), std::out_of_range);
+
+	EndTest
+}
+
---
+++ UnitTesting.h
@@ -0,0 +1,84 @@
+#pragma once
+#ifndef WILD_UNIT_TESTING_H
+#define WILD_UNIT_TESTING_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+namespace Wild
+{
+	namespace UnitTesting
+	{
+		class Test
+		{
+		public:
+
+			void Assert(bool test, const std::string &file, int line, const std::string &details = 0)
+			{
+				test ? Pass() : Fail(file, line, details);
+			}
+
+			void Pass()
+			{
+				passed++;
+				total++;
+			}
+
+			void Fail(const std::string &file, int line, const std::string &details)
+			{
+				std::cout << file << "(" << line << "): test failed, tried \"" << details << "\"" << std::endl;
+				failed++;
+				total++;
+			}
+
+			void Results()
+			{
+				std::cout << passed << " passed, " << failed << " failed, " << total << " total" << std::endl;
+			}
+
+			// Running counts for relevant stats. We'll trust the user application not to modify them.
+			int passed = 0;
+			int failed = 0;
+			int total = 0;
+		};
+
+		// Make sure that each file that includes this header is using the same object.
+		// This is useful to keep a global count of passed and failed tests across all files.
+		inline Test& AllTests()
+		{
+			static Test t = Test();
+			return t;
+		}
+	}
+}
+
+
+// Macros to make the testing process easier
+
+#define Pass() Wild::UnitTesting::AllTests().Pass();
+
+#define Fail(details) Wild::UnitTesting::AllTests().Fail(__FILE__, __LINE__, details);
+
+#define AssertTrue(x, details) Wild::UnitTesting::AllTests().Assert(x, __FILE__, __LINE__, details);
+
+static std::stringstream wildUnitTestingSs;
+#define AssertEquals(x, y) \
+	wildUnitTestingSs << x << " == " << y; \
+	AssertTrue(x == y, wildUnitTestingSs.str()); \
+	wildUnitTestingSs.str("");
+
+#define AssertThrows(code, ex) try \
+{ \
+	code; \
+	Fail(""); \
+} \
+catch (ex){ Pass(); } \
+catch (...){ Fail(""); }
+
+#define EndTest Wild::UnitTesting::AllTests().Results(); return Wild::UnitTesting::AllTests().failed;
+
+
+
+#endif
