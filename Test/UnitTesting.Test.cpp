6bf85b0bd998e841a63c8f65e9849a1d5806833f
wc-dha <david.hamilton@wildcoast.io>
2015-06-16 02:30:35 +1200

    Change to spaces instead of tabs.

--- Test/UnitTesting.Test.cpp
+++ Test/UnitTesting.Test.cpp
@@ -8,50 +8,48 @@ using namespace Wild::UnitTesting;
 
 void ThrowsInvalidArgument()
 {
-	throw std::invalid_argument("foo");
+    throw std::invalid_argument("foo");
 }
 
 void ThrowsOutOfRange()
 {
-	throw std::out_of_range("foo");
+    throw std::out_of_range("foo");
 }
 
 int main(int argc, char* argv[])
 {
-	AssertEquals(13, 13);
-	AssertEquals(true, true);
-	AssertEquals(false, false);
-	std::string s = "foo";
-	AssertEquals("foo", s);
+    AssertEquals(13, 13);
+    AssertEquals(true, true);
+    AssertEquals(false, false);
+    std::string s = "foo";
+    AssertEquals("foo", s);
 
-	Test test;
-	test.Assert(true, "filename", 1, "passed");
-	AssertEquals(1, test.passed);
-	AssertEquals(0, test.failed);
+    Test test;
+    test.Assert(true, "filename", 1, "passed");
+    AssertEquals(1, test.passed);
+    AssertEquals(0, test.failed);
 
-	std::stringstream output;
-	std::streambuf* original = std::cout.rdbuf(output.rdbuf());
+    std::stringstream output;
+    std::streambuf* original = std::cout.rdbuf(output.rdbuf());
 
-	test.Assert(false, "filename", 2, "false == false");
-	std::string text = output.str();
-	output.str("");
+    test.Assert(false, "filename", 2, "false == false");
+    std::string text = output.str();
+    output.str("");
 
-	Fail("test"); std::string failLine = std::string(__FILE__) + "(" + std::to_string(__LINE__) + ")";
-	std::string text2 = output.str();
-	
-	std::cout.rdbuf(original);
+    Fail("test"); std::string failLine = std::string(__FILE__) + "(" + std::to_string(__LINE__) + ")";
+    std::string text2 = output.str();
 
-	AssertEquals(text, "filename(2): test failed, tried \"false == false\"\n");
-	AssertEquals(text2, failLine + ": test failed, tried \"test\"\n");
+    std::cout.rdbuf(original);
 
-	AssertEquals(1, test.passed);
-	AssertEquals(1, test.failed);
+    AssertEquals(text, "filename(2): test failed, tried \"false == false\"\n");
+    AssertEquals(text2, failLine + ": test failed, tried \"test\"\n");
 
-	AssertThrows(ThrowsInvalidArgument(), std::invalid_argument);
-	AssertThrows(ThrowsInvalidArgument(), std::logic_error);		// Parent classes are also accepted
-	AssertThrows(ThrowsInvalidArgument(), std::exception);			// Parent classes are also accepted
-	AssertThrows(ThrowsOutOfRange(), std::out_of_range);
+    AssertEquals(1, test.passed);
+    AssertEquals(1, test.failed);
 
-	EndTest
+    AssertThrows(ThrowsInvalidArgument(), std::invalid_argument);
+    AssertThrows(ThrowsInvalidArgument(), std::logic_error);        // Parent classes are also accepted because they can be caught
+    AssertThrows(ThrowsInvalidArgument(), std::exception);            // Parent classes are also accepted
+    AssertThrows(ThrowsOutOfRange(), std::out_of_range);
 }
 
--- UnitTesting.h
+++ UnitTesting.h
@@ -5,53 +5,52 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <stdexcept>
 
 namespace Wild
 {
-	namespace UnitTesting
-	{
-		class Test
-		{
-		public:
+    namespace UnitTesting
+    {
+        class Test
+        {
+        public:
 
-			void Assert(bool test, const std::string &file, int line, const std::string &details = 0)
-			{
-				test ? Pass() : Fail(file, line, details);
-			}
+            void Assert(bool test, const std::string &file, int line, const std::string &details = 0)
+            {
+                test ? Pass() : Fail(file, line, details);
+            }
 
-			void Pass()
-			{
-				passed++;
-				total++;
-			}
+            void Pass()
+            {
+                passed++;
+                total++;
+            }
 
-			void Fail(const std::string &file, int line, const std::string &details)
-			{
-				std::cout << file << "(" << line << "): test failed, tried \"" << details << "\"" << std::endl;
-				failed++;
-				total++;
-			}
+            void Fail(const std::string &file, int line, const std::string &details)
+            {
+                std::cout << file << "(" << line << "): test failed, tried \"" << details << "\"" << std::endl;
+                failed++;
+                total++;
+            }
 
-			void Results()
-			{
-				std::cout << passed << " passed, " << failed << " failed, " << total << " total" << std::endl;
-			}
+            void Results()
+            {
+                std::cout << passed << " passed, " << failed << " failed, " << total << " total" << std::endl;
+            }
 
-			// Running counts for relevant stats. We'll trust the user application not to modify them.
-			int passed = 0;
-			int failed = 0;
-			int total = 0;
-		};
+            // Running counts for relevant stats. We'll trust the user application not to modify them.
+            int passed = 0;
+            int failed = 0;
+            int total = 0;
+        };
 
-		// Make sure that each file that includes this header is using the same object.
-		// This is useful to keep a global count of passed and failed tests across all files.
-		inline Test& AllTests()
-		{
-			static Test t = Test();
-			return t;
-		}
-	}
+        // Make sure that each file that includes this header is using the same object.
+        // This is useful to keep a global count of passed and failed tests across all files.
+        inline Test& AllTests()
+        {
+            static Test t = Test();
+            return t;
+        }
+    }
 }
 
 
@@ -65,14 +64,14 @@ namespace Wild
 
 static std::stringstream wildUnitTestingSs;
 #define AssertEquals(x, y) \
-	wildUnitTestingSs << x << " == " << y; \
-	AssertTrue(x == y, wildUnitTestingSs.str()); \
-	wildUnitTestingSs.str("");
+    wildUnitTestingSs << x << " == " << y; \
+    AssertTrue(x == y, wildUnitTestingSs.str()); \
+    wildUnitTestingSs.str("");
 
 #define AssertThrows(code, ex) try \
 { \
-	code; \
-	Fail(""); \
+    code; \
+    Fail(""); \
 } \
 catch (ex){ Pass(); } \
 catch (...){ Fail(""); }
